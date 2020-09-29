8809b2847aac9703533db9f5d4a6796df3bf02ae
akiencore <akiencore@qq.com>
2020-09-29 19:12:55 -0400

    E02

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public Rigidbody rb;
+
+
 	// Use this for initialization
 	void Start ()
     { // run when your game starts
-        Debug.Log("Hello, World!");
+
+        //Debug.Log("Hello, World!");
+
+        //rb.useGravity = false;
+        //rb.AddForce(0, 200, 500);
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	//void Update () {
+	void FixedUpdate () { //FixedUpdate for updating physics
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); //add a force of 2000 on z-axis
+    }
 }
