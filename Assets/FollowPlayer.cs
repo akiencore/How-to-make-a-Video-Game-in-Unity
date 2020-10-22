0aef5e8bb6975d0a602948becba46bd583ef98ef
akiencore <akiencore@qq.com>
2020-10-22 00:38:16 -0400

    E04

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //we have a "Transform" component outside (x y z axis position)
+    public Transform player;
+    public Vector3 offset; //Vector3 is a variable of 3 axis
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(player.position);
+
+        //make position of the current project same as player's position
+        //...and get back a bit with offset
+        transform.position = player.position + offset;
+    }
+}
