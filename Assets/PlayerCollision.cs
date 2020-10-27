d6b15fb92ff85c486f77345b85111178ece10c31
akiencore <akiencore@qq.com>
2020-10-27 09:16:21 -0400

    E05

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo) //call if a collision happens
+    {
+
+        //Debug.Log("We hit something!");
+        //Debug.Log(collisionInfo.collider.name); //the name of the stuff we hit
+
+        //if(collisionInfo.collider.name == "Obstacle")
+        if(collisionInfo.collider.tag == "Obstacle"){
+            
+            //Debug.Log("We hit an obstacle!");
+
+            movement.enabled = false; //stop the movement
+        }
+        
+        
+        
+    }
+
+}
