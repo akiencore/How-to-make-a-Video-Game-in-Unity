f323805c9bdc9caa347eb4eaf0223d5c1d23a50a
akiencore <akiencore@qq.com>
2020-10-28 21:12:06 -0400

    E07

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //to modify UI
+
+public class Score : MonoBehaviour
+{
+    public Transform player; //Transform is responsible for position
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0"); //int to String before assignment, shown in whole numbers
+    }
+}
